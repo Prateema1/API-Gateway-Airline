89c4612d6b097df723b5c374325289889435f546
Prateem1 <prateema.adhikari@gmail.com>
2024-01-31 12:55:56 +0530

    added rate limiter and proxy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const { ServerConfig, Logger } = require('./config');
+
+const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
 
-app.use(express.json());
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	limit: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes).
+})
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+app.use(limiter);
+
+app.use('/flightsService', createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true, pathRewrite: {'^/flightsService': '/'} }));
+app.use('/bookingService', createProxyMiddleware({ target: ServerConfig.BOOKING_SERVICE, changeOrigin: true, pathRewrite: {'^/bookingService': '/'} }));
+
 
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
  console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
- Logger.info("Successfully started the server", "root", {})
 });
+
+/**
+ * user sends request to 
+ * |
+ * localhost:3001/bookingservice/api/v1/bookings (API Gateway) localhost:4000/api/v1/bookings (booking service)
+ * 
+ * localhost:3000/api/v1/flights  (flight service)
+ */
